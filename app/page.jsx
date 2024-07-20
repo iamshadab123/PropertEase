e868a6fce2b45f4db8c203a5e1738435e6350e50
Mohammad Shadab <shadab9345@gmail.com>
2024-07-20 19:50:17 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import Hero from '@/components/Hero';
 import InfoBoxes from '@/components/InfoBoxes';
 import HomeProperties from '@/components/HomeProperties';
