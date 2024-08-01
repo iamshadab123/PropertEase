74d7b0c665040255fa801b037d2f3dc29fc882cc
Mohammad Shadab <shadab9345@gmail.com>
2024-08-01 11:56:58 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,24 +1,46 @@
+// import mongoose from 'mongoose';
+
+// let connected = false;
+
+// const connectDB = async () => {
+//   mongoose.set('strictQuery', true);
+
+//   // If the database is already connected, don't connect again
+//   if (connected) {
+//     console.log('MongoDB is already connected...');
+//     return;
+//   }
+
+//   // Connect to MongoDB
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI);
+//     connected = true;
+//     console.log('MongoDB connected...');
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// export default connectDB;
+
 import mongoose from 'mongoose';
 
-let connected = false;
-
 const connectDB = async () => {
-  mongoose.set('strictQuery', true);
+  if (mongoose.connection.readyState >= 1) return;
 
-  // If the database is already connected, don't connect again
-  if (connected) {
-    console.log('MongoDB is already connected...');
-    return;
-  }
-
-  // Connect to MongoDB
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    connected = true;
-    console.log('MongoDB connected...');
+    console.log('Connecting to MongoDB...');
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, // 30 seconds timeout
+    });
+    console.log('MongoDB connected successfully');
   } catch (error) {
-    console.log(error);
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
   }
 };
 
 export default connectDB;
+
