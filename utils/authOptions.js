06c5c600190b6c0cf3b3c75f0431c3cc840a965e
Mohammad Shadab <shadab9345@gmail.com>
2024-08-19 16:23:05 +0530

    Update authOptions.js

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,57 +1,50 @@
 import connectDB from '@/config/database';
 import User from '@/models/User';
+
 import GoogleProvider from 'next-auth/providers/google';
 
-// Configuration options for NextAuth
 export const authOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID, // Google Client ID from environment variables
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET, // Google Client Secret from environment variables
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       authorization: {
         params: {
           prompt: 'consent',
-          access_type: 'offline', 
-          response_type: 'code', // OAuth 2.0 response type
+          access_type: 'offline',
+          response_type: 'code',
         },
       },
     }),
   ],
   callbacks: {
-    // Callback invoked on successful sign-in
+    // Invoked on successful signin
     async signIn({ profile }) {
-      // 1. Connect to the database
+      // 1. Connect to database
       await connectDB();
-      
-      // 2. Check if a user with the given email already exists in the database
+      // 2. Check if user exists
       const userExists = await User.findOne({ email: profile.email });
-      
-      // 3. If the user does not exist, create a new user record in the database
+      // 3. If not, then add user to database
       if (!userExists) {
-        // Truncate the user's name if it exceeds 20 characters
+        // Truncate user name if too long
         const username = profile.name.slice(0, 20);
 
-        // Create a new user document in MongoDB
         await User.create({
           email: profile.email,
-          username, // Truncated username
-          image: profile.picture, // Profile picture URL from Google
+          username,
+          image: profile.picture,
         });
       }
-      
-      // 4. Return true to proceed with the sign-in process
+      // 4. Return true to allow sign in
       return true;
     },
-    
-    // Callback to modify the session object
+    // Modifies the session object
     async session({ session }) {
-      // 1. Retrieve the user record from the database using the session email
+      // 1. Get user from database
       const user = await User.findOne({ email: session.user.email });
-      
-      // 2. Assign the user's MongoDB ID to the session object
+      // 2. Assign the user id to the session
       session.user.id = user._id.toString();
-      
-      // 3. Return the modified session object
+      // 3. return session
       return session;
     },
   },
