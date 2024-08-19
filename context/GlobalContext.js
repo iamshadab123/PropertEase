2b9101c696c928fdbf862c8e9a8531565e0b0d20
Mohammad Shadab <shadab9345@gmail.com>
2024-08-19 16:03:07 +0530

    Update GlobalContext.js

--- context/GlobalContext.js
+++ context/GlobalContext.js
@@ -1,26 +1,30 @@
 'use client';
 import { createContext, useContext, useState } from 'react';
 
-// Create context
+// Create a context object
 const GlobalContext = createContext();
 
-// Create a provider
+// Create a provider component
 export function GlobalProvider({ children }) {
+  // State to manage unread count
   const [unreadCount, setUnreadCount] = useState(0);
 
   return (
+    // Provide context value to children components
     <GlobalContext.Provider
       value={{
-        unreadCount,
-        setUnreadCount,
+        unreadCount, // Current unread count
+        setUnreadCount, // Function to update unread count
       }}
     >
-      {children}
+      {children} {/* Render children components */}
     </GlobalContext.Provider>
   );
 }
 
-// Create a custom hook to access context
+// Custom hook to access the global context
 export function useGlobalContext() {
+  // Access the context value
   return useContext(GlobalContext);
 }
+
