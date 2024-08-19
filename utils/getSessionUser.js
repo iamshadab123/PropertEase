514c2df8e680bc4e9c01deb166420e6897844437
Mohammad Shadab <shadab9345@gmail.com>
2024-08-19 16:38:55 +0530

    Update getSessionUser.js

--- utils/getSessionUser.js
+++ utils/getSessionUser.js
@@ -1,28 +1,21 @@
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/utils/authOptions';
 
-// Function to get the currently authenticated user from the session
 export const getSessionUser = async () => {
   try {
-    // Attempt to retrieve the current server session using the NextAuth options
     const session = await getServerSession(authOptions);
 
-    // If there is no session or no user associated with the session, return null
     if (!session || !session.user) {
       return null;
     }
 
-    // Return an object containing the user details and the user's ID
     return {
-      user: session.user, // The user object from the session
-      userId: session.user.id, // The user's ID, added in the session callback
+      user: session.user,
+      userId: session.user.id,
     };
   } catch (error) {
-    // Log any errors that occur during the process
     console.error(error);
-
-    // Return null if an error occurs
     return null;
   }
-};
+}; 
 
