4f4bcde9ca19ce8f3c9c8627a44b0ed96ea3e688
Mohammad Shadab <shadab9345@gmail.com>
2024-08-19 16:37:36 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,42 +1,31 @@
 import { Schema, model, models } from 'mongoose';
 
-// Define the schema for the User model
 const UserSchema = new Schema(
   {
-    // Email address of the user, must be unique and required
     email: {
       type: String,
-      unique: [true, 'Email already exists'], // Ensures email is unique
-      required: [true, 'Email is required'], // Makes email a required field
+      unique: [true, 'Email already exists'],
+      required: [true, 'Email is required'],
     },
-    
-    // Username of the user, required field
     username: {
       type: String,
-      required: [true, 'Username is required'], // Makes username a required field
+      required: [true, 'Username is required'],
     },
-    
-    // Optional profile image URL for the user
     image: {
       type: String,
     },
-    
-    // List of properties bookmarked by the user
     bookmarks: [
       {
-        type: Schema.Types.ObjectId, // References the Property model
+        type: Schema.Types.ObjectId,
         ref: 'Property',
       },
     ],
   },
   {
-    // Automatically manage createdAt and updatedAt fields
     timestamps: true,
   }
 );
 
-// Create or retrieve the User model from mongoose models
 const User = models.User || model('User', UserSchema);
 
 export default User;
-
