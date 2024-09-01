a042e01f3a39f195e770c0f35dc99f0af51e8cc6
Mohammad Shadab <shadab9345@gmail.com>
2024-09-01 13:06:52 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -256,7 +256,14 @@ const Navbar = () => {
                 Add Property
               </Link>
             )}
-
+            <Link
+                  href='/about'
+                  className={`${
+                    pathname === '/about' ? 'bg-black' : ''
+                  } text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
+                >
+                  About
+                </Link>
             {!session &&
               providers &&
               Object.values(providers).map((provider, index) => (
